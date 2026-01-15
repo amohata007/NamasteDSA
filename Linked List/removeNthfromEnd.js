cbf7f4a986ffffcea27e68f79e28930cabe896c0
Abhishek Mohata <amohata.007@gmail.com>
2026-01-15 14:27:01 +0530

    Odd Even and Remove Nth Node

--- Linked List/removeNthfromEnd.js
+++ Linked List/removeNthfromEnd.js
@@ -11,18 +11,37 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-    let count = 0;
-    let curr=head;
-    while(curr){
-        curr=curr.next;
-        count++;
-    }
+    //uding 2 pass
+    // let count = 0;
+    // let curr=head;
+    // while(curr){
+    //     curr=curr.next;
+    //     count++;
+    // }
+    // let sentinal = new ListNode(null);
+    // sentinal.next = head;
+    // let prev = sentinal;
+    // for(let i=0;i<count-n;i++){
+    //     prev = prev.next;
+    // }
+    // prev.next = prev.next.next;
+    // return sentinal.next;
+
+
+    //Using 1 pass
     let sentinal = new ListNode(null);
     sentinal.next = head;
-    let prev = sentinal;
-    for(let i=0;i<count-n;i++){
-        prev = prev.next;
+    let first = sentinal;
+
+    for(let i=0;i<n;i++){
+        first = first.next;
     }
-    prev.next = prev.next.next;
+
+    let second = sentinal;
+    while(first && first.next){
+        first = first.next;
+        second=second.next;
+    }
+    second.next = second.next.next;
     return sentinal.next;
 };
