62b30861a5ea430a8b6aaa139b4afd528f8afe41
Abhishek Mohata <amohata.007@gmail.com>
2026-01-18 22:03:04 +0530

    Group Anagram

--- Strings/groupAnagrams2.js
+++ Strings/groupAnagrams2.js
@@ -1,2 +1,26 @@
 
 let str = ["eat","tea","tan","ate","nat","bat"];
+
+let map = {};
+
+for(let i=0;i<str.length;i++){
+    let freq = Array(26).fill(0);
+    let s = str[i];
+    for(let j=0;j<s.length;j++){
+        let index = s[j].charCodeAt() - "a".charCodeAt();
+        freq[index]++;
+    }
+
+    let key = "";
+    for(let k=0;k<26;k++){
+        key = key+String.fromCharCode(k+"a".charCodeAt(0))+freq[k];
+    }
+
+    if(!map[key]){
+        map[key] = [s];
+    }
+    else{
+        map[key].push(s);
+    }
+}
+console.log(Object.values(map));
