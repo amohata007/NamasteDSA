b4f00872cf01392f37783bddf68883917ea4bc18
Abhishek Mohata <amohata.007@gmail.com>
2026-01-22 07:40:56 +0530

    Add Two Promises

---
+++ Self/AddPromises.js
@@ -0,0 +1,20 @@
+// LC-2723
+
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    const value = await promise1;
+    const value2 = await promise2;
+    return value+value2;
+
+    // const [value1, value2] = await Promise.all([promise1, promise2]);
+    // return value1 + value2;
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
