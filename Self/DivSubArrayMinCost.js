b685c155cb339f0ff1f149e1e79f589d20ff57f9
Abhishek Mohata <amohata.007@gmail.com>
2026-02-01 08:37:12 +0530

    3010 - With Subarray as output

--- Self/DivSubArrayMinCost.js
+++ Self/DivSubArrayMinCost.js
@@ -21,3 +21,38 @@ var minimumCost = function(nums) {
     }
     return nums[0]+min+secondMin;
 };
+
+//If have to show subarray
+
+/**
+ * @param {number[]} nums
+ * @return {{ cost: number, subarrays: number[][] }}
+ */
+var minimumCostWithSubarrays = function(nums) {
+    let firstMin = Infinity, secondMin = Infinity;
+    let firstIdx = -1, secondIdx = -1;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] < firstMin) {
+            secondMin = firstMin;
+            secondIdx = firstIdx;
+            firstMin = nums[i];
+            firstIdx = i;
+        } else if (nums[i] < secondMin) {
+            secondMin = nums[i];
+            secondIdx = i;
+        }
+    }
+
+    // Ensure correct order for splitting
+    let cuts = [firstIdx, secondIdx].sort((a, b) => a - b);
+
+    let sub1 = nums.slice(0, cuts[0]);
+    let sub2 = nums.slice(cuts[0], cuts[1]);
+    let sub3 = nums.slice(cuts[1]);
+
+    return {
+        cost: nums[0] + firstMin + secondMin,
+        subarrays: [sub1, sub2, sub3]
+    };
+};
