3a876dbb0bec7d08cf6c7211da004dbff8291d59
Abhishek Mohata <amohata.007@gmail.com>
2026-02-07 17:53:27 +0530

    2181 - Merge Nodes Bw Zeros

---
+++ Self/MergeNodeBwZero.js
@@ -0,0 +1,31 @@
+//LC-2181
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var mergeNodes = function(head) {
+    let res = new ListNode();
+    let tail = res;
+    let sum = 0;
+    let curr = head.next;
+    while(curr){
+        if(curr.val===0){
+            tail.next = new ListNode(sum);
+            tail = tail.next;
+            sum = 0;
+        }
+        else{
+            sum += curr.val;
+        }
+        curr = curr.next;
+    }
+    return res.next;
+};
