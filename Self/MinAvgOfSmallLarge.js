8705ae0491d6ed873dde8aaa381b3b2f7c059fae
Abhishek Mohata <amohata.007@gmail.com>
2026-02-11 07:56:45 +0530

    3194 - Optimised Min Avg of large and small

--- Self/MinAvgOfSmallLarge.js
+++ Self/MinAvgOfSmallLarge.js
@@ -2,6 +2,8 @@
  * @param {number[]} nums
  * @return {number}
  */
+//O(n2logn)
+//O(1)
 var minimumAverage = function(nums) {
     let avg = 0;
     let smallest = Infinity;
@@ -15,5 +17,25 @@ var minimumAverage = function(nums) {
         nums.shift();
         console.log(nums)
     }
+    return smallest;
+};
+
+//Optimized
+//O(nlogn)
+//O(1)
+var minimumAverage = function(nums) {
+    nums.sort((a, b) => a - b);
+
+    let left = 0;
+    let right = nums.length - 1;
+    let smallest = Infinity;
+
+    while (left < right) {
+        const avg = (nums[left] + nums[right]) / 2;
+        smallest = Math.min(smallest, avg);
+        left++;
+        right--;
+    }
+
     return smallest;
 };
