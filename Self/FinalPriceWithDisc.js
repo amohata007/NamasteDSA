793d1e23d04100d54c8db4c43033d2cf39ca0b54
Abhishek Mohata <amohata.007@gmail.com>
2026-02-12 07:56:04 +0530

    1475 - Final price with disc Brute force

---
+++ Self/FinalPriceWithDisc.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number[]}
+ */
+//Brute force
+//O(n2)
+var finalPrices = function(prices) {
+    for(let i=0;i<prices.length;i++){
+        for(let j=i+1;j<prices.length;j++){
+            if(prices[j]<=prices[i]){
+                prices[i] = prices[i]-prices[j];
+                break;
+            }
+        }
+    }
+    return prices;
+};
+
