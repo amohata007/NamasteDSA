c6d23098481c72592005b527fb005fd7afd2ef71
Abhishek Mohata <amohata.007@gmail.com>
2026-02-14 08:35:55 +0530

    3668 - Restore Finishing Order.

---
+++ Self/RestoreFinishingOrder.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} order
+ * @param {number[]} friends
+ * @return {number[]}
+ */
+//O(n*m)
+//O(1)
+var recoverOrder = function(order, friends) {
+    let res = [];
+    for(let i=0;i<order.length;i++){
+        if(friends.includes(order[i])){
+            res.push(order[i]);
+        }
+    }
+    return res;
+};
