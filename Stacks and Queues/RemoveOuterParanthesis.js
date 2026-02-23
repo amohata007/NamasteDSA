c2abebd4d4a09aa9e7396098ea1e6f11c8ff04a5
Abhishek Mohata <amohata.007@gmail.com>
2026-02-23 15:01:19 +0530

    150 - Reverse Polish Notation

--- Stacks and Queues/RemoveOuterParanthesis.js
+++ Stacks and Queues/RemoveOuterParanthesis.js
@@ -2,24 +2,51 @@
  * @param {string} s
  * @return {string}
  */
-var removeOuterParentheses = function(s) {
+var removeOuterParentheses = function (s) {
     let stack = [];
+    let res = '';
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(") {
+            stack.push(s[i]);
+            let len = stack.length;
+            if (len > 1) {
+                res += s[i];
+            }
+        }
+        else {
+            let len = stack.length;
+            if (len > 1) {
+                res += s[i];
+            }
+            stack.pop();
+        }
+
+    }
+    return res;
+};
+
+//Without using stack
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeOuterParentheses = function(s) {
+    let level = 0;
     let res = '';
 
     for(let i=0;i<s.length;i++){
         if(s[i]==="("){
-            stack.push(s[i]);
-            let len = stack.length;
-        if(len>1){
+            ++level;
+        if(level>1){
             res += s[i];
         }
         }
         else{
-            let len = stack.length;
-        if(len>1){
+        if(level>1){
             res += s[i];
         }
-            stack.pop();
+            --level;
         }
         
     }
