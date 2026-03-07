45de4323552256084c8b7a0b84c6585b321ec9fb
Abhishek Mohata <amohata.007@gmail.com>
2026-03-07 18:30:49 +0530

    2 Stacks Post trav Iterative

--- Binary Tree/BTPostOrderTrav.js
+++ Binary Tree/BTPostOrderTrav.js
@@ -22,3 +22,20 @@ var postorderTraversal = function(root) {
     traversal(root);
     return ans;
 };
+
+//Iterative using 2 stacks
+// if(!root) return [];
+//     let s1 = [root];
+//     let s2 = [];
+
+//     while(s1.length){
+//         let curr = s1.pop();
+//         s2.push(curr);
+//         curr.left && s1.push(curr.left);
+//         curr.right && s1.push(curr.right);
+//     }
+//     let ans = [];
+//     while(s2.length){
+//         ans.push(s2.pop().val);
+//     }
+//     return ans;
