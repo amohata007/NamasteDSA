4acd90c8f22b038bab13c81b725fb289c9f9065d
Abhishek Mohata <amohata.007@gmail.com>
2026-03-09 16:24:21 +0530

    102 - Level Order Traversal

---
+++ Binary Tree/LevelOrder.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+//Uses Queues
+var levelOrder = function(root) {
+    if(!root) return [];
+    let queue = [root];
+    let ans = [];
+
+    while(queue.length){
+        let levelArr = [];
+        let levelSize = queue.length;
+        for(let i=0;i<levelSize;i++){
+        let curr = queue.shift();
+        curr.left && queue.push(curr.left);
+        curr.right && queue.push(curr.right);
+        levelArr.push(curr.val);
+        }
+        ans.push(levelArr);
+    }
+    return ans;
+};
