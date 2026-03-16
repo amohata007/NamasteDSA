b1453c5bc0ec06cb7e913d293873460bc162e657
Abhishek Mohata <amohata.007@gmail.com>
2026-03-16 11:25:31 +0530

    2331 - Evaluate Boolean Binary Tree

---
+++ Binary Tree/BooleanBT.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var evaluateTree = function(root) {
+    if(!root.right && !root.left){
+        return root.val === 1;
+    }
+    const left = evaluateTree(root.left)
+    const right = evaluateTree(root.right)
+
+    if(root.val===2){
+        return left || right;
+    }
+    else{
+        return left && right;
+    }
+};
