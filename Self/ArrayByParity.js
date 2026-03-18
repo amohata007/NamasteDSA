37895e61d83f511f4a9cd22da799f888cb51ee89
Abhishek Mohata <amohata.007@gmail.com>
2026-03-18 17:21:18 +0530

    3467 - Transform Array By Parity.

---
+++ Self/ArrayByParity.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var transformArray = function(nums) {
+    let res = [];
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]%2===0){
+            res.push(0);
+        }
+        else{
+            res.push(1);
+        }
+    }
+    return res.sort()
+};
