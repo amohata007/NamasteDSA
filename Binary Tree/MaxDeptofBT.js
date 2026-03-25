699151187f77c304c1323fd1af76b09f09fbf374
Abhishek Mohata <amohata.007@gmail.com>
2026-03-25 18:17:18 +0530

    Depth of Binary Tree

--- Binary Tree/MaxDeptofBT.js
+++ Binary Tree/MaxDeptofBT.js
@@ -12,22 +12,22 @@
  */
 //Top Down Approach
 var maxDepth = function(root) {
-    if(!root) return 0;
-    let level = 0;
-    let maxLevel = 0;
+    // if(!root) return 0;
+    // let level = 0;
+    // let maxLevel = 0;
 
-    function traverse(curr,level){
-        maxLevel = Math.max(maxLevel,level);
-        curr.left && traverse(curr.left,level+1);
-        curr.right && traverse(curr.right, level+1);
+    // function traverse(curr,level){
+    //     maxLevel = Math.max(maxLevel,level);
+    //     curr.left && traverse(curr.left,level+1);
+    //     curr.right && traverse(curr.right, level+1);
         
-    }
-    traverse(root,level);
-    return maxLevel+1;
+    // }
+    // traverse(root,level);
+    // return maxLevel+1;
 
     //Bottom up approach
-    // if(!root) return 0;
-    // let leftNode = maxDepth(root.left)
-    // let rightNode = maxDepth(root.right)
-    // return 1+Math.max(leftNode,rightNode);
+    if(!root) return 0;
+    let leftNode = maxDepth(root.left)
+    let rightNode = maxDepth(root.right)
+    return 1+Math.max(leftNode,rightNode);
 };
