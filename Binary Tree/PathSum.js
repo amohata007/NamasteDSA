1bd57f3507c83a1866b727e346719aa3c196755e
Abhishek Mohata <amohata.007@gmail.com>
2026-03-26 17:39:08 +0530

    Path SUm

--- Binary Tree/PathSum.js
+++ Binary Tree/PathSum.js
@@ -27,3 +27,28 @@ var hasPathSum = function(root, targetSum) {
     trav(root,0);
     return ans;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(curr, targetSum) {
+    if(!curr) return false;
+        if(!curr.left && !curr.right){
+            return curr.val === targetSum;
+        }
+        let sumToFind = targetSum - curr.val;
+        let leftPath = hasPathSum(curr.left, sumToFind);
+        let rightPath = hasPathSum(curr.right, sumToFind);
+        return leftPath || rightPath;
+
+};
