8fd7ef7e8c5634a336ed48578697e7dcf2ca9cae
Abhishek Mohata <amohata.007@gmail.com>
2026-03-29 16:46:44 +0530

    572 - Subtree of another tree

--- Binary Tree/SubTreeOfAnother.js
+++ Binary Tree/SubTreeOfAnother.js
@@ -11,6 +11,8 @@
  * @param {TreeNode} subRoot
  * @return {boolean}
  */
+//t - O(n*m)
+//s - O(n*m)
 var isSubtree = function (root, subRoot) {
     var isSameTree = function (p, q) {
         if (!p && !q) return true;
@@ -26,3 +28,40 @@ var isSubtree = function (root, subRoot) {
     return isSubtree(root.left, subRoot) ||
         isSubtree(root.right, subRoot);
 };
+
+var isSubtree = function(root, subRoot) {
+//     var isSameTree = function(p, q) {
+//     if(!p && !q) return true;
+//     if(!p || !q) return false;
+//     return p.val === q.val &&
+//     isSameTree(p.left,q.left) &&
+//     isSameTree(p.right,q.right);
+// };
+// if (!root) return false;
+
+//     if (isSameTree(root, subRoot)) return true;
+
+//     return isSubtree(root.left, subRoot) || 
+//            isSubtree(root.right, subRoot);
+        
+    let hash1 = serialize(root);
+    let hash2 = serialize(subRoot);
+    // console.log(hash1);
+    // console.log(hash2)
+    return hash1.includes(hash2);
+};
+
+let serialize = (root) => {
+    let hash = "";
+    let traversal = (curr) => {
+        if(!curr){
+            hash += '-#';
+            return;
+        }
+        hash += '-' + curr.val 
+        traversal(curr.left);
+        traversal(curr.right);
+    }
+    traversal(root);
+    return hash;
+}
