04e50d66dd9cc4ae1e44203d69d0c43f15446236
Abhishek Mohata <amohata.007@gmail.com>
2026-04-06 17:28:27 +0530

    1038 - BST to GST

---
+++ Binary Search Tree/BSTtoGST.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var bstToGst = function(root) {
+    let total =0;
+    let traverse = (curr) => {
+        if(!curr) return;
+        traverse(curr.right);
+        total += curr.val;
+        curr.val = total;
+        traverse(curr.left);
+    }
+    traverse(root);
+    return root;
+};
