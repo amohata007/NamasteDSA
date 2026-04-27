cb560a824f8edbdbbf0c640842fc45c038f039f1
Abhishek Mohata <amohata.007@gmail.com>
2026-04-27 08:54:10 +0530

    Date To Binary

--- Self/ConvertDateToBinary.js
+++ Self/ConvertDateToBinary.js
@@ -2,16 +2,16 @@
  * @param {string} date
  * @return {string}
  */
-var convertDateToBinary = function(date) {
+var convertDateToBinary = function (date) {
     date = date.split("-");
-    for(let i=0;i<date.length;i++){
+    for (let i = 0; i < date.length; i++) {
         date[i] = Number(date[i]).toString(2);
     }
     return date.join("-");
 };
 
 //using map
-var convertDateToBinary = function(date) {
+var convertDateToBinary = function (date) {
     return date
         .split("-")
         .map(x => Number(x).toString(2))
