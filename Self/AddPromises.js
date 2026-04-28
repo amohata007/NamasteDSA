c767af73a5bff5145824a9da540a5d20e80ef7be
Abhishek Mohata <amohata.007@gmail.com>
2026-04-28 17:57:22 +0530

    Add Promises

--- Self/AddPromises.js
+++ Self/AddPromises.js
@@ -5,10 +5,10 @@
  * @param {Promise} promise2
  * @return {Promise}
  */
-var addTwoPromises = async function(promise1, promise2) {
+var addTwoPromises = async function (promise1, promise2) {
     const value = await promise1;
     const value2 = await promise2;
-    return value+value2;
+    return value + value2;
 
     // const [value1, value2] = await Promise.all([promise1, promise2]);
     // return value1 + value2;
