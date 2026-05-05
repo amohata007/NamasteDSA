74bc2a8956a5d4d9aab0ef6a75adbb98f9826ca7
Abhishek Mohata <amohata.007@gmail.com>
2026-05-05 22:22:02 +0530

    Sort Vowel string

--- Self/SortVowelsString.js
+++ Self/SortVowelsString.js
@@ -2,12 +2,12 @@
  * @param {string} s
  * @return {string}
  */
-var sortVowels = function(s) {
-    const vowels = new Set(['a','e','i','o','u','A','E','I','O','U']);
+var sortVowels = function (s) {
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
     const storeVowels = [];
 
-    for(let char of s){
-        if(vowels.has(char)){
+    for (let char of s) {
+        if (vowels.has(char)) {
             storeVowels.push(char)
         }
     }
@@ -17,12 +17,12 @@ var sortVowels = function(s) {
     let result = "";
     let index = 0;
 
-    for(let char of s){
-        if(vowels.has(char)){
+    for (let char of s) {
+        if (vowels.has(char)) {
             result += storeVowels[index];
             index++;
         }
-        else{
+        else {
             result += char;
         }
     }
