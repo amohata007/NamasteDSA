a7f69786577d942765193341be0e2f18f507ea84
Abhishek Mohata <amohata.007@gmail.com>
2026-06-18 18:39:20 +0530

    Palindrome

--- WarmUp/Palindrome.js
+++ WarmUp/Palindrome.js
@@ -1,13 +1,13 @@
-function isPaindrome(num){
-    if(num<0) return false;
+function isPaindrome(num) {
+    if (num < 0) return false;
     let dup = num;
     let rev = 0;
-    while(num>0){
-        let rem = num%10;
-        rev = (10*rev) + rem;
-        num = Math.floor(num/10);
+    while (num > 0) {
+        let rem = num % 10;
+        rev = (10 * rev) + rem;
+        num = Math.floor(num / 10);
     }
-    return dup===rev;
+    return dup === rev;
 }
 
 let num = 121;
