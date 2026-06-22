92795ba39a353a535097082865ab5e51aec5739e
Abhishek Mohata <amohata.007@gmail.com>
2026-06-22 08:38:54 +0530

    Rev Digit

--- WarmUp/revDigit.js
+++ WarmUp/revDigit.js
@@ -21,19 +21,19 @@
  * @param {number} x
  * @return {number}
  */
-var reverse = function(x) {
+var reverse = function (x) {
     let temp = x;
     temp = Math.abs(temp);
     let rev = 0;
 
-    while(temp>0){
-        let rem = temp%10;
-        rev = (rev*10) + rem;
-        temp = Math.floor(temp/10);
+    while (temp > 0) {
+        let rem = temp % 10;
+        rev = (rev * 10) + rem;
+        temp = Math.floor(temp / 10);
     }
 
-    let digit = Math.pow(2,31);
-    if(rev>digit || rev<-digit) return 0;
+    let digit = Math.pow(2, 31);
+    if (rev > digit || rev < -digit) return 0;
 
-    return x<0 ? -rev : rev;
+    return x < 0 ? -rev : rev;
 };
