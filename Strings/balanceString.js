9cd00665505833489280e04700c7a5af172e3aa6
Abhishek Mohata <amohata.007@gmail.com>
2026-06-24 16:52:03 +0530

    Balance String

--- Strings/balanceString.js
+++ Strings/balanceString.js
@@ -5,14 +5,14 @@
 //LC - 1221
 //O(n)
 //O(1)
-var balancedStringSplit = function(s) {
+var balancedStringSplit = function (s) {
     let count = 0;
     let left = 0;
     let right = 0;
 
-    for(let i=0;i<s.length;i++){
-        s[i]==="R" ? right++ : left++;
-        if(right==left){
+    for (let i = 0; i < s.length; i++) {
+        s[i] === "R" ? right++ : left++;
+        if (right == left) {
             count++;
         }
     }
