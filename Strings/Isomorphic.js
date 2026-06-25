5a6e3c745cf93e297f7b2189ed2088f5a182c260
Abhishek Mohata <amohata.007@gmail.com>
2026-06-25 15:59:33 +0530

    Isophormic

--- Strings/Isomorphic.js
+++ Strings/Isomorphic.js
@@ -5,17 +5,17 @@
 let a = "foo";
 let b = "baa";
 
-function isIsomorphic(s,t){
-    if(s.length !== t.length) return false;
+function isIsomorphic(s, t) {
+    if (s.length !== t.length) return false;
     let mapStoT = {};
     let mapTtoS = {};
 
-    for(let i=0;i<s.length;i++){
-        if(!mapStoT[s[i]] && !mapTtoS[t[i]]){
+    for (let i = 0; i < s.length; i++) {
+        if (!mapStoT[s[i]] && !mapTtoS[t[i]]) {
             mapStoT[s[i]] = t[i];
             mapTtoS[t[i]] = s[i];
         }
-        else if(mapStoT[s[i]]!==t[i] || mapTtoS[t[i]]!==s[i]){
+        else if (mapStoT[s[i]] !== t[i] || mapTtoS[t[i]] !== s[i]) {
             return false;
         }
     };
@@ -23,4 +23,4 @@ function isIsomorphic(s,t){
     return true;
 }
 
-console.log(isIsomorphic(a,b));
+console.log(isIsomorphic(a, b));
