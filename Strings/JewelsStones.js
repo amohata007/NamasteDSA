e975968a4eb74668c2059ae7f2ce538861933df3
Abhishek Mohata <amohata.007@gmail.com>
2026-06-26 23:58:04 +0530

    Jewel stones

--- Strings/JewelsStones.js
+++ Strings/JewelsStones.js
@@ -7,17 +7,17 @@
 // TC - O(n)
 // SC - O(1) - Because sets don't have duplicates and max alphabets 26+26 that is 52 so ignored if set of 
 // numbers then O(n) but in strings its O(1) - IMPPPPPPPPP
-var numJewelsInStones = function(jewels, stones) {
+var numJewelsInStones = function (jewels, stones) {
     let storeJewels = new Set();
-    for(let i=0;i<jewels.length;i++){
+    for (let i = 0; i < jewels.length; i++) {
         storeJewels.add(jewels[i]);
     }
 
     let count = 0;
-    for(let i=0;i<stones.length;i++){
-        if(storeJewels.has(stones[i])){
+    for (let i = 0; i < stones.length; i++) {
+        if (storeJewels.has(stones[i])) {
             count++;
         }
-    }   
+    }
     return count;
 };
